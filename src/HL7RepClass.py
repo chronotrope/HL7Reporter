0f7d37cda0a9c9e5fa54fa0c94c15c3978288059
chronotrope <ruben.go.robles@gmail.com>
2012-11-13 10:00:12 -0800

    Added support for OBR-7, Observation Date/Time

--- src/HL7RepClass.py
+++ src/HL7RepClass.py
@@ -2,7 +2,7 @@
 
 ## This python class will parse HL7 ORU messages as input (first argument) and will produce a
 ## tab-delimited flat file as output (2nd argument) consisting of the following HL7 v 2.3.x fields.
-## MSH-3, MSH-5, PV1-2, PID-3, PID-5, ORC-1, ORC-2, ORC-3
+## MSH-3, MSH-5, PV1-2, PID-3, PID-5, ORC-1, ORC-2, ORC-3, OBR-4, and OBR-7
 ## To-do:  I'll continue to add HL7 fields and will also convert this
 
 
@@ -30,6 +30,7 @@ class ParseORUClass:
 	self.outFile = outFile
 	##self.inFile = self.inFile.replace("\\", "/")
 	##self.outFile = self.outFile.replace("\\", "/")
+    ## If you want finals only, then OBR-25=F
 	
 	
 	
@@ -38,10 +39,10 @@ class ParseORUClass:
     def parseORU(self):
         f = file(self.inFile, 'r')
         fileHandle = open (self.outFile, 'w')
-        header = "MSH-3\tMSH-5\tPV1-2\tPID-3\tPID-5\tORC-1\tORC-2\tORC-3\tOBR-4\n"
+        header = "MSH-3\tMSH-5\tPV1-2\tPID-3\tPID-5\tORC-1\tORC-2\tORC-3\tOBR-4\tOBR-7\n"
         print header
         fileHandle.write(header)
-        msgRow = [[],[],[],[],[], [], [],[],[]]
+        msgRow = [[],[],[],[],[], [], [],[],[],[]]
         for splitSeg in f.readlines():
 
             splitSeg  = splitSeg.split("|")
@@ -75,8 +76,10 @@ class ParseORUClass:
             if re.search("OBR", splitSeg[0]):
                 obrFour = splitSeg[4]
                 msgRow[8] = obrFour
+                obrSeven = splitSeg[7]
+                msgRow[9] = obrSeven
                 segRow = (msgRow[0],"\t",msgRow[1],"\t", msgRow[2], "\t", msgRow[3],"\t",msgRow[4],"\t"+ msgRow[5],"\t",
-                msgRow[6], "\t", msgRow[7],  "\t", msgRow[8], "\n")
+                msgRow[6], "\t", msgRow[7],  "\t", msgRow[8], "\t", msgRow[9],"\n")
                 ##function below displays object type (ie. tuple)
                 ##print segRow.__class__
                 print segRow
@@ -97,5 +100,4 @@ class ParseORUClass:
 
 
         
-
 
